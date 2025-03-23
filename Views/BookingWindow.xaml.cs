41a148f0542def6b310e1d16137785d58d8f3158
DenesTrambola <156126997+DenesTrambola@users.noreply.github.com>
2025-03-23 15:24:28 +0200

    feature: add Views

--- Views/BookingWindow.xaml.cs
+++ Views/BookingWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -6,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Muse_Travel_Manager
+namespace Muse_Travel_Manager.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for BookingWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class BookingWindow : Window
     {
-        public MainWindow()
+        public BookingWindow()
         {
             InitializeComponent();
         }
---
+++ Views/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Muse_Travel_Manager.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
